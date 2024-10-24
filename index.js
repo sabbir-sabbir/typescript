e72155f6b2a684e5dae0164840b55d61b55b6bb1
sabbir-sabbir <iosabbir7890@gmail.com>
2024-10-24 19:53:11 +0600

    commit

---
+++ index.js
@@ -0,0 +1,5 @@
+// index.ts
+function addnumbers(num1, num2) {
+    console.log(num1 + num2);
+}
+addnumbers(50, 30);
---
+++ index.ts
@@ -0,0 +1,7 @@
+// index.ts
+function addnumbers(num1: number, num2: number) {
+    console.log(num1 + num2);
+ }
+ 
+ addnumbers(50, 30);
+ 
