c8377fbe4c255f4eaec9ab268e106e8364ad58c1
sabbir-sabbir <iosabbir7890@gmail.com>
2024-11-02 19:47:56 +0600

    commit

--- build-in-type/index.js
+++ build-in-type/index.js
@@ -1,13 +1,37 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var User = /** @class */ (function () {
     function User(userName, age) {
         this.userName = userName;
         this.age = age;
     }
-    User.prototype.Display = function () {
-        console.log("Myname: ".concat(this.userName, ", MyAge: ").concat(this.age));
-    };
     return User;
 }());
+var Student = /** @class */ (function (_super) {
+    __extends(Student, _super);
+    function Student(userName, age, studentID) {
+        var _this = _super.call(this, userName, age) || this;
+        _this.studentID = studentID;
+        return _this;
+    }
+    Student.prototype.MyDisplay = function () {
+        console.log("username: ".concat(this.userName, ", age: ").concat(this.age, ", id: ").concat(this.studentID));
+    };
+    return Student;
+}(User));
 ;
-var user1 = new User("Sabbir", 25);
-user1.Display();
+var Student1 = new Student("Sabbir", 25, 101);
+Student1.MyDisplay();
--- build-in-type/index.ts
+++ build-in-type/index.ts
@@ -1,18 +1,25 @@
 class User {
-     userName: string;
-     age: number;
+    userName: string;
+    age: number;
 
-    constructor(userName: string, age: number){
+    constructor(userName: string, age: number) {
         this.userName = userName;
         this.age = age;
-
     }
-       Display(): void{
-        console.log(`Myname: ${this.userName}, MyAge: ${this.age}`)
-       }
+}
 
+class Student extends User {
+    studentID: number;
 
+    constructor(userName: string, age: number, studentID: number) {
+        super(userName, age);
+        this.studentID = studentID;
+    }
+
+    MyDisplay(): void {
+        console.log(`username: ${this.userName}, age: ${this.age}, id: ${this.studentID}`);
+    }
 };
 
-let user1 = new User("Sabbir", 25);
-user1.Display();
+let Student1 = new Student("Sabbir", 25, 101);
+Student1.studentID;
