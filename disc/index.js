8304d0410eeb4c3705777f087d866b35a409ca1e
sabbir-sabbir <iosabbir7890@gmail.com>
2024-12-19 16:24:07 +0600

    add Car type definition and log car object in TypeScript

--- disc/index.js
+++ disc/index.js
@@ -1,1 +1,10 @@
 "use strict";
+const carYear = 2001;
+const carType = "Toyota";
+const carModel = "Corolla";
+const car = {
+    year: carYear,
+    type: carType,
+    model: carModel
+};
+console.log(car);
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,19 @@
-type nyName = string;
-type myAge = number;
-type isTrue = boolean;
-
-typ Person = {
-    name: myName;
-    age: myAge;
-    single: isTrue;
+type CarYear = number
+type CarType = string
+type CarModel = string
+type Car = {
+  year: CarYear,
+  type: CarType,
+  model: CarModel
 }
+
+const carYear: CarYear = 2001
+const carType: CarType = "Toyota"
+const carModel: CarModel = "Corolla"
+const car: Car = {
+  year: carYear,
+  type: carType,
+  model: carModel
+};
+
+console.log(car);
