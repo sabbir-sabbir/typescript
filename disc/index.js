df99262ef12a0630387d795ff260f9a32029d52e
sabbir-sabbir <iosabbir7890@gmail.com>
2024-12-19 16:13:44 +0600

    remove printStatusCode function and add TypeScript types for Person

--- disc/index.js
+++ disc/index.js
@@ -1,6 +1,1 @@
 "use strict";
-function printStatusCode(code) {
-    console.log(`My status code is ${code}.`);
-}
-printStatusCode('sabbir');
-printStatusCode(404);
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+type nyName = string;
+type myAge = number;
+type isTrue = boolean;
+
+typ Person = {
+    name: myName;
+    age: myAge;
+    single: isTrue;
+}
