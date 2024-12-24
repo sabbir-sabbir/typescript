a0f363c0813ac8c0d528a6ec8a6b8d2a862d4b28
sabbir-sabbir <iosabbir7890@gmail.com>
2024-12-24 19:50:59 +0600

    remove NamedValue class and related code; add Apple function

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,33 @@
-// Basic generics
-class NamedValue<T> {
-    private _value: T | undefined;
+
+function Apple()
+
+{
+  let z = 10;
+  return true
+}
+
+Apple()
+
+
+// // Basic generics
+// class NamedValue<T> {
+//     private _value: T | undefined;
   
-    constructor(private name: string) {}
+//     constructor(private name: string) {}
   
-    public setValue(value: T) {
-      this._value = value;
-    }
+//     public setValue(value: T) {
+//       this._value = value;
+//     }
   
-    public getValue(): T | undefined {
-      return this._value;
-    }
+//     public getValue(): T | undefined {
+//       return this._value;
+//     }
   
-    public toString(): string {
-      return `${this.name}: ${this._value}`;
-    }
-  }
+//     public toString(): string {
+//       return `${this.name}: ${this._value}`;
+//     }
+//   }
   
-  let value = new NamedValue<number>('myNumber');
-  value.setValue(10);
-  console.log(value.toString()); // myNumber: 10
+//   let value = new NamedValue<number>('myNumber');
+//   value.setValue(10);
+//   console.log(value.toString()); // myNumber: 10
